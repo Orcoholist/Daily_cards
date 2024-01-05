aa1f72a0c227e8f59f8d74b5d89fd59e7f9e04ba
Orcoholist <orcoholist@mail.ru>
2024-01-06 00:37:05 +0300

    functions

---
+++ jest-03/03-func-solutions.js
@@ -0,0 +1,18 @@
+
+// Задача 1: Возведение числа в квадрат, объявите фунцию squareNumber
+const squareNumber = (number) => {
+    return number**2
+}
+
+// Задача 2: Удвоение числа, объявите фунцию doubleNumber
+const doubleNumber = (number) => {
+   return  number*2
+}
+
+// Задача 3: Увеличение числа на 1, объявите фунцию incrementNumber
+const incrementNumber = (number) => {
+    return number+1
+}
+
+
+module.exports = { squareNumber, doubleNumber, incrementNumber };
